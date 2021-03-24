f10e21bfc2eb7b5d30b44710859c590773d4849e
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2021-03-25 01:41:29 +0900

    add slickfunction
    トップ画像スライダー機能実装

--- js/common.js
+++ js/common.js
@@ -1,4 +1,18 @@
-$('.slick01').slick({ 
-    autoplay: true, 
-    dots: true, 
+// $('.slick01').slick({ 
+//     autoplay: true, 
+//     dots: true, 
+//   });
+  
+$(function(){
+  $(".slick01").slick({
+    autoplay: true,
+    speed: 1500,
+    dots: false,
+    autoplaySpeed: 3000,
+    fade: true,
+    arrow: false,
+    pauseOnHover: false,
+    arrows: false,
+    centerMode: true
   });
+});
