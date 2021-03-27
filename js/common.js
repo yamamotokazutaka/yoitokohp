5d63c7a2de6ab5475b8e54bc51741f77f5958b0f
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2021-03-27 09:03:46 +0900

    アニメ

--- js/common.js
+++ js/common.js
@@ -25,3 +25,20 @@ $(document).ready(function() {
     $(this).find(".topindex").slideUp();
   });
 });
+
+
+let targets = document.querySelectorAll('.animation_block');//アニメーションさせたい要素
+let offset = 100;//アニメーションタイミング
+
+window.addEventListener('scroll', function() {//スクロールしたとき
+
+  var scroll = window.scrollY;//スクロール量を取得
+  var h = window.innerHeight;//画面の高さを取得
+
+  for(let target of targets) {
+    var pos = target.getBoundingClientRect().top + scroll;//アニメーションさせたい要素の位置を取得
+    if (scroll > pos - h + offset) {//スクロール量 > アニメーションさせたい要素の位置
+      target.classList.add('is-animated');
+    }
+  }
+});
