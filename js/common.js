6d8e4a8fe14f0060dac35f9f8374fcddb727d71b
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2021-04-09 21:14:51 +0900

    js IE対応

--- js/common.js
+++ js/common.js
@@ -35,7 +35,8 @@ window.addEventListener('scroll', function() {//スクロールしたとき
   var scroll = window.scrollY;//スクロール量を取得
   var h = window.innerHeight;//画面の高さを取得
 
-  for(let target of targets) {
+  for(let i in targets) {
+    let target = targets[i];
     var pos = target.getBoundingClientRect().top + scroll;//アニメーションさせたい要素の位置を取得
     if (scroll > pos - h + offset) {//スクロール量 > アニメーションさせたい要素の位置
       target.classList.add('is-animated');
