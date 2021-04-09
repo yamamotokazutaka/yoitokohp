d64eaa1daeb40e41edf827ba329b8c3d0b156f1a
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2021-04-09 21:38:45 +0900

    js 対応

--- js/common.js
+++ js/common.js
@@ -35,7 +35,7 @@ window.addEventListener('scroll', function() {//スクロールしたとき
   var scroll = window.scrollY;//スクロール量を取得
   var h = window.innerHeight;//画面の高さを取得
 
-  for(let i in targets) {
+  for(var i = 0; i< targets.length; i++) {
     let target = targets[i];
     var pos = target.getBoundingClientRect().top + scroll;//アニメーションさせたい要素の位置を取得
     if (scroll > pos - h + offset) {//スクロール量 > アニメーションさせたい要素の位置
